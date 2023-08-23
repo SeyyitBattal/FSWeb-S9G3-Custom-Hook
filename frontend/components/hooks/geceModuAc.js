9644d712f3f0f24900a251095d37462e8a48a474
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-08-23 18:09:02 +0300

    All tasks are done

--- frontend/components/hooks/geceModuAc.js
+++ frontend/components/hooks/geceModuAc.js
@@ -1,8 +1,19 @@
 import { useState } from "react";
-import { localStorageKullan } from "./localStorageKullan";
 
-export const useInput = () => {
-  const [darkMode, setDarkMode] = useState(false);
+export const useInput = (key, initialValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
   const onClickHandler = () => setDarkMode(!darkMode);
-  return [darkMode, onClickHandler];
+  return [darkMode, setDarkMode, onClickHandler];
+};
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+  const setValue = (value) => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+  return [storedValue, setValue];
 };
